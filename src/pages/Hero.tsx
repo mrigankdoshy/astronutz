1ab175f9239ac8da9324031b270f7047c936826d
mrigankdoshy <mrigankdoshy@gmail.com>
2022-04-28 19:19:37 -0400

    Wording

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable global-require */
-import { Box, Button, Container, Heading, Stack, Text } from '@chakra-ui/react';
+import { Box, Container, Heading, Stack, Text } from '@chakra-ui/react';
 
 export default function Hero() {
   return (
@@ -8,43 +8,21 @@ export default function Hero() {
         <Stack
           as={Box}
           textAlign="center"
-          spacing={{ base: 8, md: 14 }}
-          py={{ base: 20, md: 36 }}
+          spacing={{ base: 8, md: 8 }}
+          py={{ base: 16, md: 64 }}
         >
           <Heading
             fontWeight={650}
-            fontSize={{ base: '2xl', sm: '4xl', md: '7xl' }}
+            fontSize={{ base: '2xl', sm: '5xl', md: '7xl' }}
             lineHeight="110%"
           >
-            AstroNUTZ, as your mom calls it <br />
+            Discover, collect, and grow your nutz <br />
           </Heading>
-          <Text color="gray.500">
-            Monetize your content by charging your most loyal readers and reward
-            them loyalty points. Give back to your loyal readers by granting
-            them access to your pre-releases and sneak-peaks.
+          <Text color="gray.500" fontSize={{ base: 'md', sm: 'lg', md: 'xl' }}>
+            A collection of 10,000 nutty NFTs - they are unique digital
+            collectibles that are exploring nodes and bytes on the Ethereum
+            blockchain.
           </Text>
-          <Stack
-            direction="column"
-            spacing={3}
-            align="center"
-            alignSelf="center"
-            position="relative"
-          >
-            <Button
-              colorScheme="green"
-              bg="green.400"
-              rounded="full"
-              px={6}
-              _hover={{
-                bg: 'green.500',
-              }}
-            >
-              Get Started
-            </Button>
-            <Button variant="link" colorScheme="blue" size="sm">
-              Learn more
-            </Button>
-          </Stack>
         </Stack>
       </Container>
     </Box>
