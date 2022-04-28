cd7aeea7c3177ac06f794312faf073be4ee62e6b
mrigankdoshy <mrigankdoshy@gmail.com>
2022-04-28 19:59:16 -0400

    Shift nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
 import { Box } from '@chakra-ui/react';
-import BackgroundImage from './assets/hero.png';
-import NavBar from './components/navigation/NavBar';
 import HomePage from './pages/HomePage';
 
 function App(): JSX.Element {
   return (
-    <Box
-      backgroundImage={`url(${BackgroundImage})`}
-      backgroundPosition="center"
-      backgroundRepeat="no-repeat"
-      backgroundSize="cover"
-      height="calc(100vh)"
-    >
-      <NavBar />
+    <Box>
       <HomePage />
     </Box>
   );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,18 @@
 import { Box } from '@chakra-ui/react';
+import BackgroundImage from '../assets/hero.png';
+import NavBar from '../components/navigation/NavBar';
 import Hero from './Hero';
 
 export default function HomePage() {
   return (
-    <Box>
+    <Box
+      backgroundImage={`url(${BackgroundImage})`}
+      backgroundPosition="center"
+      backgroundRepeat="no-repeat"
+      backgroundSize="cover"
+      height="calc(100vh)"
+    >
+      <NavBar />
       <Hero />
     </Box>
   );
