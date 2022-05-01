07604b210b3b73582494a33780bcf8622e49fc8d
mrigankdoshy <mrigankdoshy@gmail.com>
2022-05-01 17:14:00 -0400

    Align

--- src/components/subscribers/SubscribeNewsletter.tsx
+++ src/components/subscribers/SubscribeNewsletter.tsx
@@ -2,6 +2,7 @@ import { CheckIcon } from '@chakra-ui/icons';
 import {
   Button,
   Container,
+  Flex,
   FormControl,
   Heading,
   Input,
@@ -19,106 +20,113 @@ export default function SubscribeNewsletter() {
   );
 
   return (
-    <Container
-      maxWidth="4xl"
-      background={Colors.royalBlueDarkOff}
-      boxShadow="xl"
-      rounded="xl"
-      padding={16}
-      align="center"
-    >
-      <Heading as="h2" fontSize={{ base: 'xl', sm: '2xl' }} marginBottom={5}>
-        Never miss a drop!
-      </Heading>
-      <Text
-        color="gray.400"
-        fontWeight={500}
-        fontSize={{ base: 'sm', sm: 'md', md: 'md' }}
-        marginBottom={5}
+    <Flex align="center" justify="center">
+      <Container
+        maxWidth="4xl"
+        background={Colors.royalBlueDarkOff}
+        boxShadow="xl"
+        rounded="xl"
+        padding={16}
       >
-        Subscribe to our super-exclusive drop list and be the first to know
-        about Astronutz drops
-      </Text>
-      <Stack
-        as="form"
-        direction={{ base: 'row' }}
-        spacing="16px"
-        paddingX={{ base: 64, sm: 4, md: 32 }}
-        marginTop={8}
-        onSubmit={(e: FormEvent) => {
-          e.preventDefault();
-          setError(false);
-          setState('submitting');
+        <Heading
+          as="h2"
+          fontSize={{ base: 'xl', sm: '2xl' }}
+          marginBottom={5}
+          textAlign="center"
+        >
+          Never miss a drop!
+        </Heading>
+        <Text
+          color="gray.400"
+          fontWeight={500}
+          fontSize={{ base: 'sm', sm: 'md', md: 'md' }}
+          marginBottom={5}
+          textAlign="center"
+        >
+          Subscribe to our super-exclusive drop list and be the first to know
+          about Astronutz drops
+        </Text>
+        <Stack
+          as="form"
+          direction={{ base: 'row' }}
+          spacing="16px"
+          paddingX={{ base: 64, sm: 4, md: 32 }}
+          marginTop={8}
+          onSubmit={(e: FormEvent) => {
+            e.preventDefault();
+            setError(false);
+            setState('submitting');
 
-          // remove this code and implement submit logic right here
-          setTimeout(() => {
-            if (email === 'fail@example.com') {
-              setError(true);
-              setState('initial');
-              return;
-            }
+            // remove this code and implement submit logic right here
+            setTimeout(() => {
+              if (email === 'fail@example.com') {
+                setError(true);
+                setState('initial');
+                return;
+              }
 
-            setState('success');
-          }, 1000);
-        }}
-      >
-        <FormControl>
-          <Input
-            required
-            id="email"
-            type="email"
-            variant="solid"
-            placeholder="Enter your email address"
-            aria-label="Your Email"
-            rounded="full"
-            color="gray.200"
-            background="gray.700"
-            opacity={0.8}
-            borderWidth={2}
-            borderColor="gray.700"
-            value={email}
-            disabled={state !== 'initial'}
-            onChange={(e: ChangeEvent<HTMLInputElement>) =>
-              setEmail(e.target.value)
-            }
-            _placeholder={{
-              color: 'gray.400',
-            }}
-            _focus={{
-              background: 'gray.800',
-              outline: 'none',
-            }}
-          />
-        </FormControl>
-        <FormControl width={{ base: '40%' }}>
-          <Button
-            display={{ base: 'inline-flex' }}
-            width="100%"
-            colorScheme={state === 'success' ? 'green' : 'blue'}
-            isLoading={state === 'submitting'}
-            type={state === 'success' ? 'button' : 'submit'}
-            fontSize="sm"
-            fontWeight={600}
-            color={Colors.white}
-            background={Colors.royalBlueLight}
-            rounded="full"
-            _hover={{
-              background: Colors.royalBlueLightOff,
-            }}
-          >
-            {state === 'success' ? <CheckIcon /> : 'Submit'}
-          </Button>
-        </FormControl>
-      </Stack>
-      <Text
-        marginTop={4}
-        textAlign="center"
-        color={error ? 'red.500' : 'gray.500'}
-      >
-        {error
-          ? 'Oh no an error occured! 😢 Please try again.'
-          : "You won't receive any spam! ✌️"}
-      </Text>
-    </Container>
+              setState('success');
+            }, 1000);
+          }}
+        >
+          <FormControl>
+            <Input
+              required
+              id="email"
+              type="email"
+              variant="solid"
+              placeholder="Enter your email address"
+              aria-label="Your Email"
+              rounded="full"
+              color="gray.200"
+              background="gray.700"
+              opacity={0.8}
+              borderWidth={2}
+              borderColor="gray.700"
+              value={email}
+              disabled={state !== 'initial'}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setEmail(e.target.value)
+              }
+              _placeholder={{
+                color: 'gray.400',
+              }}
+              _focus={{
+                background: 'gray.800',
+                outline: 'none',
+              }}
+            />
+          </FormControl>
+          <FormControl width={{ base: '40%' }}>
+            <Button
+              display={{ base: 'inline-flex' }}
+              width="100%"
+              colorScheme={state === 'success' ? 'green' : 'blue'}
+              isLoading={state === 'submitting'}
+              type={state === 'success' ? 'button' : 'submit'}
+              fontSize="sm"
+              fontWeight={600}
+              color={Colors.white}
+              background={Colors.royalBlueLight}
+              rounded="full"
+              _hover={{
+                background: Colors.royalBlueLightOff,
+              }}
+            >
+              {state === 'success' ? <CheckIcon /> : 'Submit'}
+            </Button>
+          </FormControl>
+        </Stack>
+        <Text
+          marginTop={4}
+          textAlign="center"
+          color={error ? 'red.500' : 'gray.500'}
+        >
+          {error
+            ? 'Oh no an error occured! 😢 Please try again.'
+            : "You won't receive any spam! ✌️"}
+        </Text>
+      </Container>
+    </Flex>
   );
 }
